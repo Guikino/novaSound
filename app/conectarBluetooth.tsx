c4f083c18bc9d2bd30f09e1fcddc3523e2160bf9
Guikino <macedoguilherme55@gmail.com>
2026-01-20 13:31:17 -0300

    add page pageConnectBluetooth

---
+++ app/conectarBluetooth.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Text } from 'tamagui'
+
+export default function ConectarBluetooth() {
+  return (
+    <>
+      <Text>Conectar Bluetooth</Text>
+    </>
+  )
+}
