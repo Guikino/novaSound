db486e3cddd0dd306fe5f0bd668962c4dd06a770
Guikino <macedoguilherme55@gmail.com>
2026-01-20 12:58:56 -0300

    Revert "adjust"
    This reverts commit 0bf64c78d1e08d7f4247d7d4c4eb37c16c7cf12c.

--- components/Equalizer.tsx
+++ components/Equalizer.tsx
@@ -161,10 +161,10 @@ export default function Equalizer({ variant = "solid", onScrollToggle }: Equaliz
         <ScrollView horizontal showsHorizontalScrollIndicator={false}>
           <XStack  gap="$2">
             {PRESETS.map((preset) => (
-              <Button  key={preset}>
+              <Button  >
                 <TamaguiStack
                 onPress={() => applyPreset(preset)}
-                
+                key={preset}
                   bg="$gray4"
                   px="$3"
                   py="$1"
